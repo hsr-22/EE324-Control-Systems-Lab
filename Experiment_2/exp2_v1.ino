941d764563f57c235bec2a516d060d4e598f9366
Harsh Roniyar <112925148+hsr-22@users.noreply.github.com>
2024-08-29 15:12:35 +0530

    exp2 day1

---
+++ Experiment_2/exp2_v1.ino
@@ -0,0 +1,394 @@
+/*
+ * AMT22_SPI_Sample_Code.ino
+ * Company: CUI Inc.
+ * Author: Jason Kelly
+ * Version: 2.0.1.0
+ * Date: August 20, 2019
+ * 
+ * This sample code can be used with the Arduino Uno to control the AMT22 encoder.
+ * It uses SPI to control the encoder and the the Arduino UART to report back to the PC
+ * via the Arduino Serial Monitor.
+ * For more information or assistance contact CUI Inc for support.
+ * 
+ * After uploading code to Arduino Uno open the open the Serial Monitor under the Tools 
+ * menu and set the baud rate to 115200 to view the serial stream the position from the AMT22.
+ * 
+ * Arduino Pin Connections
+ * SPI Chip Select Enc 0:   Pin  2
+ * SPI Chip Select Enc 1:   Pin  3
+ * SPI MOSI                 Pin 11
+ * SPI MISO                 Pin 12
+ * SPI SCLK:                Pin 13
+ * 
+ * 
+ * AMT22 Pin Connections
+ * Vdd (5V):                Pin  1
+ * SPI SCLK:                Pin  2
+ * SPI MOSI:                Pin  3
+ * GND:                     Pin  4
+ * SPI MISO:                Pin  5
+ * SPI Chip Select:         Pin  6
+ * 
+ * 
+ * This is free and unencumbered software released into the public domain.
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ * 
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
+/* Include the SPI library for the arduino boards */
+#include <SPI.h>
+
+/* Serial rates for UART */
+#define BAUDRATE        115200
+
+/* SPI commands */
+#define AMT22_NOP       0x00
+#define AMT22_RESET     0x60
+#define AMT22_ZERO      0x70
+
+/* Define special ascii characters */
+#define NEWLINE         0x0A
+#define TAB             0x09
+
+/* We will use these define macros so we can write code once compatible with 12 or 14 bit encoders */
+#define RES12           12
+#define RES14           14
+
+/* SPI pins */
+#define ENC_0            2
+#define ENC_1            3
+#define SPI_MOSI        51
+#define SPI_MISO        50
+#define SPI_SCLK        52
+
+#define msDelay 10
+
+// double A[4][4];
+// double B[4][1];
+// double C[4][4];
+
+double k[4]={  -3.1623  , 57.1757  , -1.9815  ,  7.5060};
+
+void setup() 
+{
+  //Set the modes for the SPI IO
+  pinMode(SPI_SCLK, OUTPUT);
+  pinMode(SPI_MOSI, OUTPUT);
+  pinMode(SPI_MISO, INPUT);
+  pinMode(ENC_0, OUTPUT);
+  pinMode(ENC_1, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  //pinMode(10, OUTPUT);
+  //Initialize the UART serial connection for debugging
+  Serial.begin(BAUDRATE);
+
+  //Get the CS line high which is the default inactive state
+  digitalWrite(ENC_0, HIGH);
+  digitalWrite(ENC_1, HIGH);
+
+  //set the clockrate. Uno clock rate is 16Mhz, divider of 32 gives 500 kHz.
+  //500 kHz is a good speed for our test environment
+  //SPI.setClockDivider(SPI_CLOCK_DIV2);   // 8 MHz
+  //SPI.setClockDivider(SPI_CLOCK_DIV4);   // 4 MHz
+  //SPI.setClockDivider(SPI_CLOCK_DIV8);   // 2 MHz
+  //SPI.setClockDivider(SPI_CLOCK_DIV16);  // 1 MHz
+  SPI.setClockDivider(SPI_CLOCK_DIV32);    // 500 kHz
+  //SPI.setClockDivider(SPI_CLOCK_DIV64);  // 250 kHz
+  //SPI.setClockDivider(SPI_CLOCK_DIV128); // 125 kHz
+  
+  //start SPI bus
+  SPI.begin();
+}
+
+void loop() 
+{
+  //create a 16 bit variable to hold the encoders position
+  long StartingTheta=getPositionSPI(ENC_1, RES14);
+  delay(msDelay);
+
+  long int theta;
+  long int thetaOld;
+  long int alpha;
+  long int alphaOld;
+  //let's also create a variable where we can count how many times we've tried to obtain the position in case there are errors
+  long int attempts;
+  long start_alpha=(long)getPositionSPI(ENC_0, RES14);
+  double dTheta=0;
+  double dAlpha=0;
+
+  double dT;
+
+  long time=0;
+
+  double Output=0;
+
+  uint16_t AlphaRead=0;
+  uint16_t ThetaRead=0;
+
+
+
+  while(1)
+  {
+    //set attemps counter at 0 so we can try again if we get bad position    
+    attempts = 0;
+
+    //this function gets the encoder position and returns it as a uint16_t
+    //send the function either res12 or res14 for your encoders resolution
+    AlphaRead = getPositionSPI(ENC_0, RES14);
+    alpha= (long)AlphaRead-start_alpha;
+    // alpha*=-1;
+
+    dT=msDelay*(1e-3);
+    dAlpha=(double)((alpha-alphaOld)/(dT));
+
+    //if the position returned was 0xFFFF we know that there was an error calculating the checksum
+    //make 3 attempts for position. we will pre-increment attempts because we'll use the number later and want an accurate count
+    while (alpha == 0xFFFF && ++attempts < 3)
+    {
+      AlphaRead = getPositionSPI(ENC_0, RES14); //try again
+      alpha = (long)AlphaRead-start_alpha;
+    }
+
+    if (alpha == 0xFFFF) //position is bad, let the user know how many times we tried
+    {
+      //Serial.print("Encoder 0 error. Attempts: ");
+      //Serial.print(attempts, DEC); //print out the number in decimal format. attempts - 1 is used since we post incremented the loop
+      //Serial.write(NEWLINE);
+    }
+    else //position was good, print to serial stream
+    {
+      
+      //Serial.print("Encoder 0: ");
+      //Serial.print(alpha, DEC); //print the position in decimal format
+      //Serial.write(NEWLINE);
+    }
+
+    attempts = 0;
+
+    ThetaRead = getPositionSPI(ENC_1, RES14); 
+    theta = ThetaRead-StartingTheta;
+
+    dTheta=((double)(theta-thetaOld)/dT);
+
+    while (theta == 0xFFFF && ++attempts < 3)
+    {
+      ThetaRead = getPositionSPI(ENC_1, RES14); //try again
+      theta = ThetaRead-StartingTheta;
+    }
+    //Serial.println(theta*360/16384);
+    //Serial.println(alpha*360/16400);
+    if (theta == 0xFFFF) //position is bad, let the user know how many times we tried
+    {
+      //Serial.print("Encoder 1 error. Attempts: ");
+      //Serial.print(attempts, DEC); //print out the number in decimal format. attempts - 1 is used since we post incremented the loop
+      //Serial.write(NEWLINE);
+    }
+    else //position was good, print to serial stream
+    {
+      
+      //Serial.print("Encoder 1: ");
+      //Serial.print(theta, DEC); //print the position in decimal format
+      //Serial.write(NEWLINE);
+    }
+   
+     Output = -0.5*(k[0]*theta + k[1]*alpha + k[2]*dTheta + k[3]*dAlpha)*360/16400;
+     
+     
+ //     -1.0000   59.0483   -0.9963    4.2415 
+
+
+//
+//    if(abs(Output)<120) Output=abs(Output)/Output*120;
+//    if(abs(Output)>255) Output=abs(Output)/Output*255;
+Serial.print("THETA: ");
+Serial.print(theta, DEC); //print the position in decimal format
+Serial.write(NEWLINE);
+Serial.print("ALPHA: ");
+Serial.print(alpha, DEC); //print the position in decimal format
+Serial.write(NEWLINE);
+Serial.print(Output);
+    if(Output>=0)
+    {
+//      digitalWrite(9,1);
+      analogWrite(8,0);
+      if(Output>255) Output=255;
+      analogWrite(9,abs(Output));
+    }
+    else
+    {
+     // digitalWrite(10,1);
+      analogWrite(9,0);
+      if(Output<-255) Output=-255;
+      analogWrite(8,abs(Output));
+    }
+    thetaOld=theta;
+    alphaOld=alpha;
+    delay(msDelay);
+  }
+}
+
+double map(double val, int M1, int M2, int O1, int O2)
+{
+  if(val<M1) return -254;
+  if(val>M2) return 254;
+  double result = (val-M1)/((M2-M1)*1.0);
+  result = result*(O2-O1) + O1;
+  return (int)result;
+}
+
+
+float filter(long input, double alpha) {
+    static long output = 0;                        // Output value from previous iteration, initialized to 0
+    output = alpha * output + (1 - alpha) * input;  // Filter input
+    return (long)output;
+}
+
+void loop1()
+{
+  uint16_t encoderPosition;
+
+  encoderPosition = getPositionSPI(ENC_1, RES14); 
+  Serial.print(encoderPosition, DEC);
+  Serial.write(NEWLINE);
+  delay(50);
+}
+
+/*
+ * This function gets the absolute position from the AMT22 encoder using the SPI bus. The AMT22 position includes 2 checkbits to use
+ * for position verification. Both 12-bit and 14-bit encoders transfer position via two bytes, giving 16-bits regardless of resolution.
+ * For 12-bit encoders the position is left-shifted two bits, leaving the right two bits as zeros. This gives the impression that the encoder
+ * is actually sending 14-bits, when it is actually sending 12-bit values, where every number is multiplied by 4. 
+ * This function takes the pin number of the desired device as an input
+ * This funciton expects res12 or res14 to properly format position responses.
+ * Error values are returned as 0xFFFF
+ */
+uint16_t getPositionSPI(uint8_t encoder, uint8_t resolution)
+{
+  uint16_t currentPosition;       //16-bit response from encoder
+  bool binaryArray[16];           //after receiving the position we will populate this array and use it for calculating the checksum
+
+  //get first byte which is the high byte, shift it 8 bits. don't release line for the first byte
+  currentPosition = spiWriteRead(AMT22_NOP, encoder, false) << 8;   
+
+  //this is the time required between bytes as specified in the datasheet.
+  //We will implement that time delay here, however the arduino is not the fastest device so the delay
+  //is likely inherantly there already
+  delayMicroseconds(3);
+
+  //OR the low byte with the currentPosition variable. release line after second byte
+  currentPosition |= spiWriteRead(AMT22_NOP, encoder, true);        
+
+  //run through the 16 bits of position and put each bit into a slot in the array so we can do the checksum calculation
+  for(int i = 0; i < 16; i++) binaryArray[i] = (0x01) & (currentPosition >> (i));
+
+  //using the equation on the datasheet we can calculate the checksums and then make sure they match what the encoder sent
+  if ((binaryArray[15] == !(binaryArray[13] ^ binaryArray[11] ^ binaryArray[9] ^ binaryArray[7] ^ binaryArray[5] ^ binaryArray[3] ^ binaryArray[1]))
+          && (binaryArray[14] == !(binaryArray[12] ^ binaryArray[10] ^ binaryArray[8] ^ binaryArray[6] ^ binaryArray[4] ^ binaryArray[2] ^ binaryArray[0])))
+    {
+      //we got back a good position, so just mask away the checkbits
+      currentPosition &= 0x3FFF;
+    }
+  else
+  {
+    currentPosition = 0xFFFF; //bad position
+  }
+
+  //If the resolution is 12-bits, and wasn't 0xFFFF, then shift position, otherwise do nothing
+  if ((resolution == RES12) && (currentPosition != 0xFFFF)) currentPosition = currentPosition >> 2;
+
+  return currentPosition;
+}
+
+/*
+ * This function does the SPI transfer. sendByte is the byte to transmit. 
+ * Use releaseLine to let the spiWriteRead function know if it should release
+ * the chip select line after transfer.  
+ * This function takes the pin number of the desired device as an input
+ * The received data is returned.
+ */
+uint8_t spiWriteRead(uint8_t sendByte, uint8_t encoder, uint8_t releaseLine)
+{
+  //holder for the received over SPI
+  uint8_t data;
+
+  //set cs low, cs may already be low but there's no issue calling it again except for extra time
+  setCSLine(encoder ,LOW);
+
+  //There is a minimum time requirement after CS goes low before data can be clocked out of the encoder.
+  //We will implement that time delay here, however the arduino is not the fastest device so the delay
+  //is likely inherantly there already
+  delayMicroseconds(3);
+
+  //send the command  
+  data = SPI.transfer(sendByte);
+  delayMicroseconds(3); //There is also a minimum time after clocking that CS should remain asserted before we release it
+  setCSLine(encoder, releaseLine); //if releaseLine is high set it high else it stays low
+  
+  return data;
+}
+
+/*
+ * This function sets the state of the SPI line. It isn't necessary but makes the code more readable than having digitalWrite everywhere 
+ * This function takes the pin number of the desired device as an input
+ */
+void setCSLine (uint8_t encoder, uint8_t csLine)
+{
+  digitalWrite(encoder, csLine);
+}
+
+/*
+ * The AMT22 bus allows for extended commands. The first byte is 0x00 like a normal position transfer, but the 
+ * second byte is the command.  
+ * This function takes the pin number of the desired device as an input
+ */
+void setZeroSPI(uint8_t encoder)
+{
+  spiWriteRead(AMT22_NOP, encoder, false);
+
+  //this is the time required between bytes as specified in the datasheet.
+  //We will implement that time delay here, however the arduino is not the fastest device so the delay
+  //is likely inherantly there already
+  delayMicroseconds(3); 
+  
+  spiWriteRead(AMT22_ZERO, encoder, true);
+  delay(250); //250 second delay to allow the encoder to reset
+}
+
+/*
+ * The AMT22 bus allows for extended commands. The first byte is 0x00 like a normal position transfer, but the 
+ * second byte is the command.  
+ * This function takes the pin number of the desired device as an input
+ */
+void resetAMT22(uint8_t encoder)
+{
+  spiWriteRead(AMT22_NOP, encoder, false);
+
+  //this is the time required between bytes as specified in the datasheet.
+  //We will implement that time delay here, however the arduino is not the fastest device so the delay
+  //is likely inherantly there already
+  delayMicroseconds(3); 
+  
+  spiWriteRead(AMT22_RESET, encoder, true);
+  
+ // delay(250); //250 second delay to allow the encoder to start back up
+}
---
+++ Experiment_2/lqr_model.m
@@ -0,0 +1,47 @@
+% This is how we roll
+M_p = 0.027;
+l_p = 0.153;
+L_p = 0.191;
+r = 0.08260;
+J_m = 3e-5;
+M_arm = 0.028;
+g = 9.810;
+J_eq = 1.23e-4;
+J_p = 1.1e-4;
+B_eq = 0;
+B_p = 0;
+R_m = 3.30;
+K_t = 0.02797;
+K_m = 0.02797;
+%%%%%%%%%%%%%%%%%%%%%%%%
+denom = J_p * J_eq + M_p * J_eq * (l_p^2) + J_p *M_p *(r^2);
+var1 = (r * ((M_p * l_p)^2) * g)/(denom);
+var2 = K_t * K_m * (J_p + M_p * (l_p^2))/(denom * R_m);
+var3 = M_p * l_p * g * (J_eq + M_p * (r^2))/denom;
+var4 = -M_p * l_p * K_t * r * K_m/(denom * R_m);
+var5 = K_t * (J_p + M_p * (l_p^2))/(denom * R_m);
+var6 = M_p * l_p * K_t * r/(denom * R_m);
+
+A = [0 0     1     0; 
+     0 0     0     1;
+     0 var1 -var2  0;
+     0 var3  var4  0];
+B = [ 0;
+      0;
+      var5;
+      var6];
+C = [1 0 0 0 ;0 1 0 0;0 0 1 0;0 0 0 1];   
+D = [0;0;0;0];
+%x is actually [theta, alpha, theta', alpha'] 
+%where theta is arm, alpha is pendulum
+Q = [   10    0   0    0;
+        0      50     0    0;
+        0      0   1     0;
+        0      0   0    2];
+R = 1;
+
+N=0;
+   
+[K, S, P] = lqr(A, B, Q, R, N);
+sys1 = ss(A-B*K,B,C,D);
+step(sys1)
